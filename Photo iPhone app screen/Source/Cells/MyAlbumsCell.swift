fe6707d05d218a9f22979bf0a60f769af5eac084
Yzee <yan.zhigurs@mail.ru>
2023-01-17 19:08:04 +0400

    adding options in MyAlbums file

--- Photo iPhone app screen/Source/Cells/MyAlbumsCell.swift
+++ Photo iPhone app screen/Source/Cells/MyAlbumsCell.swift
@@ -12,5 +12,79 @@ class MyAlbums: UICollectionViewCell{
     
     static let identifier = "MyAlbums"
     
+    // MARK: - Outlets
     
+    private lazy var featuredTitle: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15)
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.textColor = .systemGray
+        return label
+    }()
+    
+    private let image: UIImageView = {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFit
+        imageView.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        return imageView
+    }()
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("ERROR")
+    }
+    
+    // MARK: - Setup
+    
+    private func setupHierarchy() {
+        addSubview(image)
+        addSubview(featuredTitle)
+        addSubview(descriptionLabel)
+    }
+    
+    private func setupLayout() {
+        
+        image.snp.makeConstraints { make in
+            make.width.equalTo(170)
+            make.height.equalTo(170)
+            make.left.top.bottom.equalTo(contentView)
+        }
+        
+        featuredTitle.snp.makeConstraints { make in
+            make.top.equalTo(image.snp.bottom).offset(0)
+            make.centerX.equalTo(image).offset(0)
+            make.left.equalTo(contentView)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(featuredTitle.snp.bottom).offset(0)
+            make.centerX.equalTo(image).offset(0)
+            make.left.equalTo(contentView)
+        }
+        
+    }
+    
+    func configuration(model: Model) {
+        self.featuredTitle.text = model.mainTitle
+        self.descriptionLabel.text = model.description
+        self.image.image = UIImage(named: model.image)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.image.image = nil
+    }
 }
