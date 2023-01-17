3da213b819d4a56fc1df8d4ef1eb9d2590565ac4
Yzee <yan.zhigurs@mail.ru>
2023-01-17 18:34:16 +0400

    add Model file

---
+++ Photo iPhone app screen/Source/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  Photo iPhone app screen
+//
+//  Created by Ян Жигурс on 17.01.2023.
+//
+
+struct CompositionalModel: Hashable {
+    var mainTitle: String?
+    var description: String?
+    var image: String
+}
+
+extension CompositionalModel {
+    static let modelsArray = [[]]
+}
