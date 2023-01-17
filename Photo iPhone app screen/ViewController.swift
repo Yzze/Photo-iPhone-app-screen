3e43d5c1fd41b7c22a33098c0948cece8ada06c6
Yzee <yan.zhigurs@mail.ru>
2023-01-17 17:58:02 +0400

    Initial Commit

---
+++ Photo iPhone app screen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo iPhone app screen
+//
+//  Created by Ян Жигурс on 17.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
