50fce675e0ba5dd116d1d14d41f3767453e79f09
Yzee <yan.zhigurs@mail.ru>
2023-01-17 19:41:58 +0400

    add parameters in shared albums cell file

--- Photo iPhone app screen/Source/Cells/SharedAlbumsCell.swift
+++ Photo iPhone app screen/Source/Cells/SharedAlbumsCell.swift
@@ -10,4 +10,52 @@ import UIKit
 class SharedAlbums: UICollectionViewCell {
     
     static let identifier = "SharedAlbums"
+    
+    // MARK: - Outlets
+    
+    private let image: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 10
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        clipsToBounds = true
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Error in Cell")
+    }
+    
+    // MARK: - Setup
+    
+    private func setupHierarchy() {
+        contentView.addSubview(image)
+    }
+    
+    private func setupLayout() {
+        image.snp.makeConstraints { make in
+            make.width.equalTo(200)
+            make.height.equalTo(200)
+            make.left.top.right.bottom.equalTo(contentView)
+        }
+    }
+    
+    // MARK: - Configuration
+    
+    func configuration(model: Model) {
+        self.image.image = UIImage(named: model.image)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.image.image = nil
+    }
 }
